3f23b710ab08e4d59c7324d5be7e2a068449bfac
amrendrayd <amrendrayd3@gmail.com>
2024-07-31 17:09:00 +0530

    Custom Api response and error handling

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,19 @@
+const asyncHaldler = (requestHandler) => {
+    (req, res, next) => {
+        Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err))
+    }
+}
+
+
+export {asyncHaldler}
+
+// const asyncHaldler = (fn) => async (req, res, next) => {
+//     try {
+//         await fn(req, res, next);
+//     } catch (error) {
+//         res.status(err.code || 500).json({
+//             success: false,
+//             message: err.message
+//         });
+//     }
+// }
