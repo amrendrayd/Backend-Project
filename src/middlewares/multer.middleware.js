a3ec82a680eeb673e7267254f6a5b8607b67b0ab
amrendrayd <amrendrayd3@gmail.com>
2024-08-03 00:18:21 +0530

    multer middleware configure

---
+++ src/middlewares/multer.middleware.js
@@ -0,0 +1,18 @@
+import multer from "multer";
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, "./public/temp")
+    },
+    filename: function (req, file, cb) {
+    
+      cb(null, )
+    }
+  })
+
+  
+export const upload = multer(
+    {
+        storage: storage
+    }
+);
