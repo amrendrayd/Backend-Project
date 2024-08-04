afe069cea9bdbfa1cf2f225a692af3301ca4095a
amrendrayd <amrendrayd3@gmail.com>
2024-08-04 23:44:54 +0530

    check host undefined error

--- src/db/index.js
+++ src/db/index.js
@@ -6,6 +6,7 @@ const connectDB = async () => {
     try {
         const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
         console.log(`\n MongoDB connected !! DB HOST: ${connectionInstance.Connection.host}`);
+        
     } catch (error) {
         console.log("MONGODB Connection error ", error);
         process.exit(1)
