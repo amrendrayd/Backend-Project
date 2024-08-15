e37906b830fd5cbf15ebb8ff6bc6a0096396e93a
amrendrayd <amrendrayd3@gmail.com>
2024-08-16 00:36:54 +0530

    bug fixed

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -47,7 +47,7 @@ router.route("/update-account").patch(verifyJWT, updateAccountDetails)
 
 router.route("/avatar").patch(verifyJWT, upload.single("avatar"), updateUserAvatar)
 
-router.route("/cover-image").patch(verifyJWT, upload.single("/coverImage"), updateUserCoverImage)
+router.route("/cover-image").patch(verifyJWT, upload.single("coverImage"), updateUserCoverImage)
 
 router.route("/c/:username").get(verifyJWT, getUserChannelProfile)
 
